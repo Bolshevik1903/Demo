94372582120180eef7ffc471afb9c7858a8ffd9d
LenninHuaman <u20201b975@upc.edu.pe>
2023-03-30 00:45:25 -0500

    Added Login.kt for the login app

---
+++ app/src/main/java/pe/edu/upc/demo/ui/screens/login/login.kt
@@ -0,0 +1,51 @@
+package pe.edu.upc.demo.ui.screens.login
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun login() {
+    Column(
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        var username = remember { mutableStateOf(TextFieldValue()) }
+        var password = remember { mutableStateOf(TextFieldValue()) }
+
+        Text(text = "Login", color = Color.Blue, fontSize = 36.sp)
+        Spacer(modifier = Modifier.height(20.dp))
+
+        TextField(value = username.value, onValueChange = { username.value = it }, placeholder = { Text(text = "Username") })
+        Spacer(modifier = Modifier.height(20.dp))
+
+        TextField(value = password.value, onValueChange = { password.value = it }, placeholder = { Text(text = "Password") }, visualTransformation = PasswordVisualTransformation())
+
+        Box(modifier = Modifier.padding(40.dp,0.dp,40.dp,0.dp)) {
+            Button(onClick = { /*TODO*/ }, shape = RoundedCornerShape(50.dp)) {
+                Text(text = "Sign in")
+            }
+        }
+
+        Box(modifier = Modifier.padding(40.dp,0.dp,40.dp,0.dp)) {
+            Button(onClick = { /*TODO*/ }, shape = RoundedCornerShape(50.dp)) {
+                Text(text = "Sign up")
+            }
+        }
+    }
+}
